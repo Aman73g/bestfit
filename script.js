0be2ba2f125439bb461557f2909a91d2d84e3466
Amanuel <Aman73g@gmail.com>
2020-12-06 16:28:33 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,126 @@
+var main=document.getElementById("main");
+main.innerHTML+='<div class="slider"><!-- The first slide --><div class="item"><img src="best-gym-near-me.jpg"></div><!-- The second slide --><div class="item"><img src="Best-HIIT-workout-apps-to-choose-from.jpg"></div><!-- The third slide --><div class="item"><img src="ce9a12653dff3b7e046f4508b6e3f11e102543dc.jpg"></div><!-- Arrow buttons --><a class="previous" onclick="previousSlide()">&#10094;</a><a class="next" onclick="nextSlide()">&#10095;</a></div> '
+ main.innerHTML+='<img id="my_pic" src="display1.png">';
+    
+   main.innerHTML+='<h1 id="intro">hi,Welcome<span id="name">Best Fitness Center</span></h1>';    
+    
+    main.innerHTML+='<p id="job"> ‘All progress takes place outside the comfort zone.’</p>'; 
+    
+  main.innerHTML+='<hr bgcolor="black">';    
+    
+  main.innerHTML+='<p id="description">  Fitness is defined as the quality or state of being fit. Around 1950, perhaps consistent with the Industrial Revolution and the treatise of World War II, the term "fitness" increased in  western vernacular by a factor of ten. The modern definition of fitness describes either a person or machine`s ability to perform a specific function or a holistic definition of human adaptability to cope with various situations. This has led to an interrelation of human fitness and attractiveness that has mobilized global fitness and fitness equipment industries. Regarding specific function, fitness is attributed to persons who possess significant aerobic or anaerobic     ability, i.e. endurance or strength. A well-rounded fitness program improves a person in all  aspects of fitness compared to practising only one, such as only cardio/respiratory endurance  or only weight training.</p>';   
+    
+    main.innerHTML+='<a href="www.gmail.com" style="text-decoration:none;"> <input id="btn_contact" type="button" value="email bestfitness@gmail.com"></a>';    
+
+
+function home(){
+  main.innerHTML='';
+  main.innerHTML+='<div class="slider"><!-- The first slide --><div class="item"><img src="best-gym-near-me.jpg"></div><!-- The second slide --><div class="item"><img src="Best-HIIT-workout-apps-to-choose-from.jpg"></div><!-- The third slide --><div class="item"><img src="ce9a12653dff3b7e046f4508b6e3f11e102543dc.jpg"></div><!-- Arrow buttons --><a class="previous" onclick="previousSlide()">&#10094;</a><a class="next" onclick="nextSlide()">&#10095;</a></div> '
+  main.innerHTML+='<img id="my_pic" src="display1.png">';
+     
+    main.innerHTML+='<h1 id="intro">hi,Welcome<span id="name">Best Fitness Center</span></h1>';    
+     
+     main.innerHTML+='<p id="job"> ‘All progress takes place outside the comfort zone.’</p>'; 
+     
+   main.innerHTML+='<hr bgcolor="black">';    
+     
+   main.innerHTML+='<p id="description">  Fitness is defined as the quality or state of being fit. Around 1950, perhaps consistent with the Industrial Revolution and the treatise of World War II, the term "fitness" increased in  western vernacular by a factor of ten. The modern definition of fitness describes either a person or machine`s ability to perform a specific function or a holistic definition of human adaptability to cope with various situations. This has led to an interrelation of human fitness and attractiveness that has mobilized global fitness and fitness equipment industries. Regarding specific function, fitness is attributed to persons who possess significant aerobic or anaerobic     ability, i.e. endurance or strength. A well-rounded fitness program improves a person in all  aspects of fitness compared to practising only one, such as only cardio/respiratory endurance  or only weight training.</p>';   
+     
+     main.innerHTML+='<a href="www.gmail.com" style="text-decoration:none;"> <input id="btn_contact" type="button" value="email bestfitness@gmail.com"></a>';    
+ }
+
+function  assignment1(){
+    main.innerHTML='';
+    
+    main.innerHTML+='<h1 id="skill_h">Schedule</h1>';
+    
+      main.innerHTML+='<table id="customers"><caption><h3  id="skill_h">table for phase 1 training schedule</h3></caption><tr><th></th><th>Monday</th><th>Tuesday</th><th>Wednesday</th><th>Thursday</th><th>Friday</th><th>Saturday</th><th>Sunday</th></tr><tr><th>Week 1</th><td>Chest&Abs</td><td>Biceps</td><td>Legs&Abs</td><td>shoulders</td><td>Triceps</td><td>Backs</td><td>Rest Day</td></tr><tr><th>Week 2</th><td>Chest&Abs</td><td>Biceps</td><td>Legs&Abs</td><td>shoulders</td><td>Triceps</td><td>Backs</td><td>Rest Day</td></tr><tr><th>Week 3</th><td>Chest&Abs</td><td>Biceps</td><td>Legs&Abs</td><td>shoulders</td><td>Triceps</td><td>Backs</td><td>Rest Day</td></tr><tr><th>Week 4</th><td>Chest&Abs</td><td>Biceps</td><td>Legs&Abs</td><td>shoulders</td><td>Triceps</td><td>Backs</td><td>Rest Day</td></tr></table>';
+      main.innerHTML+='<h1 id="skill_h"><a href="Progress checker.pdf" style="text-decoration:none;">Click Here</a>to download progress checking table</h1>' 
+}
+function  assignment2(){
+  main.innerHTML='';
+  
+  main.innerHTML+='<h1 id="skill_h">Diet Plan</h1>';
+  main.innerHTML+='<h1 id="skill_h">Losing Weight</h1>';
+    main.innerHTML+='<img src="Weight-loss-diet-plan.png" height="500px" width="1332px">';
+    main.innerHTML+='<h1 id="skill_h">Healthy</h1>';
+      main.innerHTML+='<img src="c182e06c12acc219cbf65b3465d4fa2a.jpg" height="500px" width="1332px">';
+}
+function  assignment3(){
+  main.innerHTML='';
+  
+  main.innerHTML+='<h1 id="skill_h">Packages</h1>';
+  main.innerHTML+='<h1 id="skill_h">Single</h1>';
+    main.innerHTML+='<div class="flex-container"><div class="card"><div class="container"><h4><b>1 Month</b></h4><p>1.000 Birr</p><p>All Day Training</p><p>1 Time sauna/month</p></div></div><div class="card"><div class="container"><h4><b>3 Month</b></h4><p>2.700 Birr</p><p>All Day Training</p><p>1 Time sauna/month</p><p>2 yoga class</p></div></div><div class="card"><div class="container"><h4><b>6 Month</b></h4><p>4,950 Birr</p><p>All Day Training</p><p>2 Time sauna/month</p><p>5 yoga class</p></div></div><div class="card"><div class="container"><h4><b>1 year</b></h4><p>8,900 Birr</p><p>All Day Training</p><p>3 Time sauna/month</p><p>11 yoga class</p><p>5 self Defence</p></div></div></div>';
+    main.innerHTML+='<h1 id="skill_h">Couples</h1>';
+    main.innerHTML+='<div class="flex-container"><div class="card"><div class="container2"><h4><b>1 Month</b></h4><p>1.500 Birr</p><p>All Day Training</p><p>1 Time sauna/month</p></div></div><div class="card"><div class="container2"><h4><b>3 Month</b></h4><p>4,900 Birr</p><p>All Day Training</p><p>1 Time sauna/month</p><p>2 yoga class</p></div></div><div class="card"><div class="container2"><h4><b>6 Month</b></h4><p>9,100 Birr</p><p>All Day Training</p><p>2 Time sauna/month</p><p>5 yoga class</p></div></div><div class="card"><div class="container2"><h4><b>1 year</b></h4><p>16,900 Birr</p><p>All Day Training</p><p>3 Time sauna/month</p><p>11 yoga class</p><p>5 self Defence</p></div></div></div>';
+
+  }
+function  assignment4(){
+  main.innerHTML='';
+  main.innerHTML+='<h3>Contact Form</h3><div class="container"><form action="/action_page.php"><label for="fname">First Name</label><input type="text" id="fname" name="firstname" placeholder="Your name.."><label for="lname">Last Name</label><input type="text" id="lname" name="lastname" placeholder="Your last name.."><label for="country">Country</label><select id="country" name="country"><option value="australia">Australia</option><option value="canada">Canada</option>        <option value="usa">USA</option></select><label for="subject">Subject</label><textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea><input type="submit" value="Submit"></form></div></body></html>';
+}
+function contact_me(){
+  main.innerHTML='<p id="contact_me_space"></p>'; 
+   main.innerHTML+='<div class="column-2"><h1>About me</h1><p>Hi! I`m Amanuel Getachew,I am 2021 graduate of BSC in construction technology and management from Addis Ababa university EIABC, I am very sociable and solution-oriented person I am curios for things around me and student of life. This site is a demonstration website done as an assignment after completing class on how to develop static website on visual studio code using HTML and CSS" which walks you through building and customizing this website from start to finish.<div style="float:right"><div style="width:300px">LinkedIn profile name (link): -  https://www.linkedin.com/in/amanuel-getachew-617011155</div><div>Email address: - Aman73g@gmail.com</div><div>Address: - Addis Ababa, Ethiopia </div><div>Phone number +251-920-74-50-33</div></div></div><div class="column-2 large-profile"><img id="pic" src="abt me pic.jpg"></div>'; 
+}function generalFact(){
+  main.innerHTML='<p id="contact_me_space"></p>'; 
+  main.innerHTML+='<h1 id="skill_h">General Facts On ur workout</h1>';
+  main.innerHTML+='<iframe width="1332" height="500" src="https://www.youtube.com/embed/e5EM9RfyGcU" title="General Facts On ur workout"></iframe>'; 
+  main.innerHTML+='<h1 id="skill_h">General Facts On ur workout</h1>';
+  main.innerHTML+='<iframe width="1332" height="500" src="https://www.youtube.com/embed/-WfM_QNRkDc" title="W3Schools Free Online Web Tutorials"></iframe>'; 
+  main.innerHTML+='<h1 id="skill_h">General Facts On ur workout diet</h1>';
+  main.innerHTML+='<iframe width="1332" height="500" src="https://www.youtube.com/embed/CxktmQ3zJOA" title="W3Schools Free Online Web Tutorials"></iframe>'; 
+}
+
+
+
+function about_me(){
+    main.innerHTML='';
+     main.innerHTML+='<div  id="bio"><h1 class="ab_me" style="margin-bottom: 0px;">Biography</h1><p style="margin-top: 0px;">born in 1989 april 6 i was rasied in addis abeba the capital city of ethiopia.life as a child was like an ordinary life of an ethiopian child. i have lived all my life in addis.i have joined severral schools while growing up till i joined bahar dar university which i am currently learning.</p></div>';
+    main.innerHTML+='<div id="my_pic_about_me"><img id="about_me_img" src="ff.jpg"></div>';
+    main.innerHTML+='<div id="Hobby"><h1 class="ab_me" style="margin-bottom: 0px;">hobby</h1><p style="margin-top: 0px;">i like to read books on programing languges concerning on web design and other professions.other than this listening to music,watching movies and hanging out with my friends are my hobies</p></div>';
+    main.innerHTML+='<div id="edu"><h1 class="ab_me" style="margin-bottom: 0px;">Education</h1><p style="margin-top: 0px;">start from first grade kidus markos is the name of the school that i have learned for 8 years.after this higer 12 and yekatit 12 are the secondary and preparatory schools that i have learned. then after passing the university enterance exam bahar dar university has become my current school wehere i have been a part of for 3 years</p></div>';
+   
+}
+
+
+
+
+/* Setting the default slide start index: */
+let slideIndex = 1;
+/* We call the function that is implemented below: */
+showSlides(slideIndex);
+/* Increase the index by 1 - show the next slide: */
+function nextSlide() {
+    showSlides(slideIndex += 1);
+}
+/* Decrease the index by 1 - show the previous slide: */
+function previousSlide() {
+    showSlides(slideIndex -= 1);  
+}
+/* Set the current slide: */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+/* Flip function: */
+function showSlides(n) {
+    let i;
+    /* We refer to the elements with the class name "item", that is, to the pictures: */
+    let slides = document.getElementsByClassName("item");
+    
+    /* Checking the number of slides: */
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+  
+    /* Loop through each slide in a for loop: */
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }
+    /* Making an element block: */
+    slides[slideIndex - 1].style.display = "block";    
+}
